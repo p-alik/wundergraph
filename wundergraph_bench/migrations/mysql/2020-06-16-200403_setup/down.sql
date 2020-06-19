a4bbd2042f266f4e53875bd89b0467b2de063f70
Alexei Pastuchov <alexei.pastuchov@pyur.com>
2020-06-19 13:06:17 +0200

    add mysql migration sql files

---
+++ wundergraph_bench/migrations/mysql/2020-06-16-200403_setup/down.sql
@@ -0,0 +1,17 @@
+-- This file should undo anything in `up.sql`
+DROP TABLE film_categorys;
+DROP TABLE film_actors;
+DROP TABLE films;
+DROP TABLE actors;
+DROP TABLE categories;
+DROP TABLE playlist_tracks;
+DROP TABLE playlists;
+DROP TABLE invoice_lines;
+DROP TABLE invoices;
+DROP TABLE customers;
+DROP TABLE employees;
+DROP TABLE tracks;
+DROP TABLE genres;
+DROP TABLE albums;
+DROP TABLE artists;
+DROP TABLE media_types;
